7b6e2668dd4638ad57d1ff71bc87125377a2e55b
Kaith Rogers <krogers@catalyte.io>
2020-01-13 13:00:37 -0700

    Updates

---
+++ precache-manifest.6c03fcf0c364d02ca30882d7886be4ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439c96c208c2c0f267c24eca81d97e71",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "172641aa0082fdf5133c",
+    "url": "/monsters-rolodex/static/css/main.899d400e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "172641aa0082fdf5133c",
+    "url": "/monsters-rolodex/static/js/main.5c42d92a.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
